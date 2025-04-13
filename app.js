153e77935fa8957258652f7f76ad593f10b51687
HarshitGautam1 <harshiitgautamm@gmail.com>
2025-04-13 12:40:02 +0530

    hello endpoint added

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 const PORT = 7000;
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
 
 app.listen(PORT , () =>{
   console.log('Server is Listening !!');
